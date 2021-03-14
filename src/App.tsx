59af1f21a08415fadd2b0cdc1d7275728a4521df
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-13 19:25:37 -0500

    testing fade

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {Button, CssBaseline} from '@material-ui/core';
+import FadeIn from 'react-fade-in';
 
 function App() {
-  return (
-    <h1>Hello World! This site is under construction 🚧</h1>
-  );
+    return (
+        <CssBaseline>
+            <FadeIn>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+                <h1>Hello World! This site is under construction 🚧</h1>
+                <Button onClick={onClickFn} color="secondary">Hello World</Button>
+            </FadeIn>
+        </CssBaseline>
+    );
+}
+
+const onClickFn = () => {
+    console.error('=============>test');
 }
 
 export default App;
