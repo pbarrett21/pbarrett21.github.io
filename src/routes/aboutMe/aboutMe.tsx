0373a7733f26b7203266483cfc8cc76f0e1ccf4d
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-23 23:43:48 -0400

    begin about

--- src/routes/aboutMe/aboutMe.tsx
+++ src/routes/aboutMe/aboutMe.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <h1>About Me</h1>
+        <div className='m-4 '>
+            <img className='rounded shadow-md'
+                 src={require('./../../resources/cup-of-coffee.jpeg').default}
+                 alt='macbook and coffee'/>
+            <div className='md:flex md:flex-col md:items-center'>
+                <h1 className='text-xl font-semibold mt-6 mb-6 md:ml-auto md:mr-auto'>Hey there, I'm Paul 👋</h1>
+                <h1 className='text-lg font-normal'>I am a software developer dedicated to
+                    constant improvement and exploration</h1>
+            </div>
+        </div>
     );
 }
 
