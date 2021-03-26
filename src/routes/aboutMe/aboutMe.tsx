0f7b78bf2787443bed29580db41542c20c2eefc6
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-26 12:58:34 -0400

    update about me, wip

--- src/routes/aboutMe/aboutMe.tsx
+++ src/routes/aboutMe/aboutMe.tsx
@@ -3,11 +3,36 @@ import React from 'react';
 const AboutMe = () => {
     return (
         <div className='max-w-full md:max-w-2xl lg:max-w-2xl m-4'>
-            <div className='md:flex md:flex-col md:items-center'>
-                <h1 className='text-xl font-semibold mt-6 mb-6 md:ml-auto md:mr-auto'>Hey there, I'm Paul 👋</h1>
-                <h1 className='text-lg font-normal'>I am a software developer dedicated to
-                    constant improvement and exploration</h1>
-            </div>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Career</h1>
+            <p className='text-lg font-normal'>
+                I am a software developer with over 2 years of professional experience. I
+                have been immersed in an agile development environment and have been fortunate to work with
+                fantastic teammates on many interesting projects while using modern technologies. I have
+                enjoyed being able to participate and contribute in new product discovery and framings, inceptions,
+                and design plannings and then create and bring our vision to life.
+            </p>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Education</h1>
+            <p className='text-lg font-normal'>
+                B.S in Computer Science with minor in Mathematics
+            </p>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Hobbies and Interests</h1>
+            <p className='text-lg font-normal'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
+                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
+                pretium rhoncus
+            </p>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Links</h1>
+            <p className='text-lg font-normal'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
+                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
+                pretium rhoncus
+            </p>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Contact</h1>
+            <p className='text-lg font-normal'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
+                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
+                pretium rhoncus
+            </p>
         </div>
     );
 }
