95ca28b6262dfb9ada64890a033b2966721bbdb6
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-27 01:38:34 -0400

    update about

--- src/routes/aboutMe/aboutMe.tsx
+++ src/routes/aboutMe/aboutMe.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './about-me-styles.scss';
 
 const AboutMe = () => {
     return (
@@ -8,30 +9,37 @@ const AboutMe = () => {
                 I am a software developer with over 2 years of professional experience. I
                 have been immersed in an agile development environment and have been fortunate to work with
                 fantastic teammates on many interesting projects while using modern technologies. I have
-                enjoyed being able to participate and contribute in new product discovery and framings, inceptions,
-                and design plannings and then create and bring our vision to life.
+                enjoyed being able to participate and contribute in the planning of new products and then to create and
+                bring that vision to life.
             </p>
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Education</h1>
-            <p className='text-lg font-normal'>
-                B.S in Computer Science with minor in Mathematics
-            </p>
+            <div>
+                <p className='text-lg font-medium'>B.S. in Computer Science with Minor in Mathematics</p>
+                <p className='text-lg font-normal'>University of Kentucky, 2015 - 2019</p>
+            </div>
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Hobbies and Interests</h1>
             <p className='text-lg font-normal'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
-                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
-                pretium rhoncus
+                I have many hobbies and interests outside of my professional life!
             </p>
+            <ul className='list-inside pt-4 pl-4'>
+                <li>Mountain Biking 🚴</li>
+                <li>Hiking 🌲</li>
+                <li>Photography 📷</li>
+                <li>Paddleboarding 🏄‍♂️</li>
+                <li>Cats 🐈</li>
+            </ul>
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Links</h1>
             <p className='text-lg font-normal'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
-                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
-                pretium rhoncus
+                Here are some useful or relevant links:
             </p>
-            <h1 className='font-semibold text-2xl mt-10 mb-6'>Contact</h1>
+
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>Resume</h1>
             <p className='text-lg font-normal'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a neque at risus dignissim volutpat
-                at et elit. Nulla elit ante, mattis eget imperdiet semper, dapibus ac risus. Aenean volutpat, purus ac
-                pretium rhoncus
+                Feel free to take a look at my
+                <a className='font-medium underline ml-1 hover:text-green-600'
+                   href='https://drive.google.com/file/d/1S6ykXqenu7QUbQdcJkXWTPL9j2sHRam7/view?usp=sharing'
+                   rel='noreferrer'
+                   target='_blank'>resume</a>
             </p>
         </div>
     );
