ef06607b726d35d0a78df854d5455a2eb665a80b
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-29 16:52:12 -0400

    wip cvs

--- src/cvs/cvs.tsx
+++ src/cvs/cvs.tsx
@@ -12,7 +12,50 @@ const Cvs = () => {
                     significantly during that
                     time. I have enjoyed being able to participate and contribute at every stage of the product
                     lifecycle from initial planning all the way to delivering MVP.</p>
-                <h1 className='font-semibold mt-4'>Non-technical Responsibilities and Scope</h1>
+
+                <h1 className='font-semibold mt-10'>Technical Responsibilities and Information</h1>
+                <h2 className='font-semibold ml-4 mt-4'>Technologies Used</h2>
+                <p className='ml-4'>Angular, SpringBoot, Oracle SQL databases, Typescript, add more here</p>
+                <h2 className='font-semibold ml-4 mt-4'>Notable Libraries Used</h2>
+                <p className='ml-4'>Spectator, NGRX, AgGrid, add more here</p>
+                <h2 className='font-semibold ml-4 mt-4'>XP (Extreme Programming)</h2>
+                <p className='ml-4'>I have been a part of an XP team since working at CVS. XP is a software development
+                    methodology similar to many other Agile methodologies. Some of the core concepts that I have been
+                    exposed to include TDD (test driven development), Continuous Integration, and paired
+                    programming. Learn more about XP
+                    <a className='underline hover:text-green-600 ml-1'
+                       href='https://en.wikipedia.org/wiki/Extreme_programming'
+                       rel='noreferrer'
+                       target='_blank'>here.</a>
+                </p>
+
+                <h1 className='font-semibold mt-10'>Accomplishments and Other Roles</h1>
+                <h2 className='font-semibold ml-4 mt-4'>Contribute to Proprietary Angular Component Library</h2>
+                <p className='ml-4'>I have enjoyed being able to contribute many new features, fix bugs, and
+                    troubleshoot on the CVS ACL. I have learned a lot about libraries, versioning, and more from this
+                    experience!</p>
+                <h2 className='font-semibold ml-4 mt-4'>Accessibility Champion</h2>
+                <p className='ml-4'>I have championed the effort to bring greater accessibility standards to all
+                    products in our lab. This has included sharing knowledge how to make apps more accessible,
+                    integrating better accessibility standards into story creation / general workflow, showcasing
+                    empathy building exercises, following
+                    <a className='underline hover:text-green-600 ml-1 mr-1'
+                       href='https://www.w3.org/TR/WCAG21/'
+                       rel='noreferrer'
+                       target='_blank'>W3 WCAG</a>
+                    standards and more.</p>
+                <h2 className='font-semibold ml-4 mt-4'>Upgrade security to Auth Code Flow</h2>
+                <p className='ml-4'>I took the initiative to upgrade the security for our application from Implicit to
+                    <a className='underline hover:text-green-600 ml-1 mr-1'
+                       href='https://auth0.com/docs/flows/authorization-code-flow'
+                       rel='noreferrer'
+                       target='_blank'>Auth Code Flow.</a>
+                    Implementing this change involved work spanning from configuration on the front end
+                    to the back end as well as SSO setup and more. </p>
+                <h2 className='font-semibold ml-4 mt-4'>Give Presentations</h2>
+                <p className='ml-4'>Spectator, SOLID, KT, LNL, AgGrid</p>
+
+                <h1 className='font-semibold mt-10'>Non-technical Responsibilities and Scope</h1>
                 <h2 className='font-semibold ml-4 mt-4'>Product Lifecycle</h2>
                 <p className='ml-4'>I have enjoyed a large degree of inclusion to every stage of the product lifecycle.
                     I have been able
