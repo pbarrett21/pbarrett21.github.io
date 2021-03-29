25a7ff076be83e2cebf27f35f3484fd0a8fc8120
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-29 17:10:37 -0400

    cvs v1 done

--- src/cvs/cvs.tsx
+++ src/cvs/cvs.tsx
@@ -14,10 +14,11 @@ const Cvs = () => {
                     lifecycle from initial planning all the way to delivering MVP.</p>
 
                 <h1 className='font-semibold mt-10'>Technical Responsibilities and Information</h1>
-                <h2 className='font-semibold ml-4 mt-4'>Technologies Used</h2>
-                <p className='ml-4'>Angular, SpringBoot, Oracle SQL databases, Typescript, add more here</p>
-                <h2 className='font-semibold ml-4 mt-4'>Notable Libraries Used</h2>
-                <p className='ml-4'>Spectator, NGRX, AgGrid, add more here</p>
+                <h2 className='font-semibold ml-4 mt-4'>Technologies and Tools Used</h2>
+                <p className='ml-4'>Angular, SpringBoot, Oracle SQL databases, Typescript, GitLab, Flyway, Lombok,
+                    Hibernate, Spring Cloud Data Flow, Pivotal Tracker, Pivotal Cloud Foundry, Jenkins, Spring JPA</p>
+                <h2 className='font-semibold ml-4 mt-4'>Notable Libraries/Frameworks Used</h2>
+                <p className='ml-4'>Spectator, NgRx, RxJS, AgGrid, JUnit, Mockito</p>
                 <h2 className='font-semibold ml-4 mt-4'>XP (Extreme Programming)</h2>
                 <p className='ml-4'>I have been a part of an XP team since working at CVS. XP is a software development
                     methodology similar to many other Agile methodologies. Some of the core concepts that I have been
@@ -26,14 +27,13 @@ const Cvs = () => {
                     <a className='underline hover:text-green-600 ml-1'
                        href='https://en.wikipedia.org/wiki/Extreme_programming'
                        rel='noreferrer'
-                       target='_blank'>here.</a>
+                       target='_blank'>here</a>.
                 </p>
 
                 <h1 className='font-semibold mt-10'>Accomplishments and Other Roles</h1>
                 <h2 className='font-semibold ml-4 mt-4'>Contribute to Proprietary Angular Component Library</h2>
-                <p className='ml-4'>I have enjoyed being able to contribute many new features, fix bugs, and
-                    troubleshoot on the CVS ACL. I have learned a lot about libraries, versioning, and more from this
-                    experience!</p>
+                <p className='ml-4'>I have enjoyed being able to contribute many new features and bugfixes on the CVS
+                    ACL. I have learned a lot about libraries, versioning, and more from this experience!</p>
                 <h2 className='font-semibold ml-4 mt-4'>Accessibility Champion</h2>
                 <p className='ml-4'>I have championed the effort to bring greater accessibility standards to all
                     products in our lab. This has included sharing knowledge how to make apps more accessible,
@@ -46,14 +46,26 @@ const Cvs = () => {
                     standards and more.</p>
                 <h2 className='font-semibold ml-4 mt-4'>Upgrade security to Auth Code Flow</h2>
                 <p className='ml-4'>I took the initiative to upgrade the security for our application from Implicit to
-                    <a className='underline hover:text-green-600 ml-1 mr-1'
+                    <a className='underline hover:text-green-600 ml-1'
                        href='https://auth0.com/docs/flows/authorization-code-flow'
                        rel='noreferrer'
-                       target='_blank'>Auth Code Flow.</a>
+                       target='_blank'>Auth Code Flow</a>.
                     Implementing this change involved work spanning from configuration on the front end
-                    to the back end as well as SSO setup and more. </p>
+                    to the back end as well as SSO setup and more. This was another great learning experience!</p>
                 <h2 className='font-semibold ml-4 mt-4'>Give Presentations</h2>
-                <p className='ml-4'>Spectator, SOLID, KT, LNL, AgGrid</p>
+                <p className='ml-4'>I have given presentations in order to share knowledge with other engineers on
+                    various topics including
+                    <a className='underline hover:text-green-600 ml-1 mr-1'
+                       href='https://github.com/ngneat/spectator'
+                       rel='noreferrer'
+                       target='_blank'>Spectator</a>
+                    and
+                    <a className='underline hover:text-green-600 ml-1'
+                       href='https://www.ag-grid.com/'
+                       rel='noreferrer'
+                       target='_blank'>AgGrid</a>
+                    !
+                </p>
 
                 <h1 className='font-semibold mt-10'>Non-technical Responsibilities and Scope</h1>
                 <h2 className='font-semibold ml-4 mt-4'>Product Lifecycle</h2>
