74f4241871c6ee6eca8c28eb290313cd27829a62
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-29 16:14:16 -0400

    add to cvs

--- src/cvs/cvs.tsx
+++ src/cvs/cvs.tsx
@@ -8,33 +8,23 @@ const Cvs = () => {
                 <h1 className='head-container md:ml-16 md:text-7xl'>CVS</h1>
             </div>
             <div className='m-4'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget tellus lacus. Donec
-                    consequat
-                    accumsan purus sit amet cursus. Ut ultrices nisi quis lorem dapibus, sit amet consectetur est
-                    facilisis.
-                    Aenean volutpat et turpis vitae condimentum. Suspendisse sed ultrices mi. Phasellus dignissim ipsum
-                    eget
-                    mi rutrum aliquet. Aliquam imperdiet elit tincidunt, semper mi at, sodales felis. Suspendisse
-                    potenti.
-                    Nam eget eleifend diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac
-                    tristique diam. Nullam sollicitudin in quam nec tincidunt. Nulla ac nibh nisl. Nulla pretium, orci
-                    sit
-                    amet ultricies malesuada, nisi magna pharetra ipsum, non tincidunt tortor dui venenatis justo.
-                    Aliquam
-                    eu augue a nulla laoreet commodo. In et mollis justo.
-
-                    Etiam elementum at urna convallis dictum. Vestibulum fermentum id purus sit amet feugiat. Nulla
-                    facilisi. In pulvinar leo mauris, eget scelerisque odio auctor in. Proin finibus auctor mi in porta.
-                    Curabitur finibus sem et rhoncus maximus. Proin eros est, pulvinar ut felis et, imperdiet auctor
-                    ante.
-                    Aliquam nec quam purus. Vestibulum posuere felis in tellus fringilla pulvinar. Cras nec purus sed
-                    ipsum
-                    scelerisque posuere eget ac est. In nisl arcu, malesuada ac velit eget, ullamcorper commodo ante.
-                    Proin
-                    aliquet sed lectus consequat ullamcorper. Mauris a risus vel dolor posuere imperdiet a id nunc.
-                    Curabitur at tempus odio. Nunc semper luctus ante, nec consectetur quam luctus ut. In egestas sapien
-                    a
-                    quam mollis feugiat.</p>
+                <p className='font-semibold'>I have been a full-stack engineer at CVS for over 2 years and have grown
+                    significantly during that
+                    time. I have enjoyed being able to participate and contribute at every stage of the product
+                    lifecycle from initial planning all the way to delivering MVP.</p>
+                <h1 className='font-semibold mt-4'>Non-technical Responsibilities and Scope</h1>
+                <h2 className='font-semibold ml-4 mt-4'>Product Lifecycle</h2>
+                <p className='ml-4'>I have enjoyed a large degree of inclusion to every stage of the product lifecycle.
+                    I have been able
+                    to participate in the initial stages of new products and have been able to provide engineering
+                    insight and suggestions during Discovery and Framing sessions, Inceptions, and Design Studios. I
+                    have been able to directly interact with and to form professional relationships with our business
+                    partners. </p>
+                <h2 className='font-semibold ml-4 mt-4'>Conduct Interviews and Provide Mentorship</h2>
+                <p className='ml-4'>I have welcomed many new engineers and teammates and have taken on the role of a
+                    mentor in order to bring them up to speed in respect to code bases, practices, and XP rituals.
+                    Additionally, I have taken the responsibility of an interviewer and have evaluated prospective
+                    engineers and designers.</p>
             </div>
         </div>
     );
