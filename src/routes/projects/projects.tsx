b0f2ad84e46699de9f99520fcfe41bf5f3041f0a
Paul Barrett <paulg.barrett4@gmail.com>
2021-03-29 22:49:53 -0400

    wip projects

--- src/routes/projects/projects.tsx
+++ src/routes/projects/projects.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ReactComponent as SiteIcon } from './../../resources/logo-white.svg';
 
 const Projects = () => {
     return (
         <div className='max-w-full md:max-w-2xl lg:max-w-2xl m-4'>
-            Projects
+            Under Construction 🚧
         </div>
     );
 }
