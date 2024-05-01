268519582aa1e00e1acbc6bd38f8886287d4b3ae
pbarrett21 <paulg.barrett4@gmail.com>
2024-05-01 17:34:14 -0400

    remove unused dependencies and update yoe

--- src/cvs/cvs.tsx
+++ src/cvs/cvs.tsx
@@ -1,94 +1,167 @@
-import React, {useContext} from 'react';
-import './cvs-styles.scss';
-import ThemeContext from '../theme-context';
+import React, { useContext } from 'react'
+import './cvs-styles.scss'
+import ThemeContext from '../theme-context'
 
 const Cvs = () => {
-
-    const theme = useContext(ThemeContext).theme;
+    const theme = useContext(ThemeContext).theme
 
     return (
-        <div className={theme === 'dark' ?
-            'text-white w-full md:max-w-2xl lg:max-w-2xl'
-            : 'w-full md:max-w-2xl lg:max-w-2xl'}>
+        <div
+            className={
+                theme === 'dark'
+                    ? 'text-white w-full md:max-w-2xl lg:max-w-2xl'
+                    : 'w-full md:max-w-2xl lg:max-w-2xl'
+            }
+        >
             <div className='w-full flex cvs-banner'>
                 <h1 className='head-container md:ml-16 md:text-7xl'>CVS</h1>
             </div>
             <div className='m-4'>
-                <p className='font-semibold'>I have been a full-stack engineer at CVS for 3 years and have grown
-                    significantly during that
-                    time. I have enjoyed being able to participate and contribute at every stage of the product
-                    lifecycle from initial planning all the way to delivering MVP.</p>
-
-                <h1 className='font-semibold mt-10'>Technical Responsibilities and Information</h1>
-                <h2 className='font-semibold ml-4 mt-4'>Technologies and Tools Used</h2>
-                <p className='ml-4'>Typescript, Oracle SQL databases, GitLab, Flyway, Lombok,
-                    Hibernate, Spring Cloud Data Flow, Pivotal Tracker, Pivotal Cloud Foundry, Jenkins, Spring JPA</p>
-                <h2 className='font-semibold ml-4 mt-4'>Notable Libraries/Frameworks Used</h2>
-                <p className='ml-4'>Spectator, NgRx, RxJS, AgGrid, JUnit, Mockito</p>
-                <h2 className='font-semibold ml-4 mt-4'>Agile</h2>
-                <p className='ml-4'>I have been a part of an agile team since working at CVS. Some of the core concepts that I have been
-                    exposed to include TDD (test driven development), Continuous Integration/Continuous Deployment, paired
-                    programming and much more.
+                <p className='font-semibold'>
+                    I have been a full-stack engineer at CVS for 5 years and
+                    have grown significantly during that time. I have enjoyed
+                    being able to participate and contribute at every stage of
+                    the product lifecycle from initial planning all the way to
+                    delivering MVP.
                 </p>
 
-                <h1 className='font-semibold mt-10'>Accomplishments and Other Roles</h1>
-                <h2 className='font-semibold ml-4 mt-4'>Contribute to Proprietary Component Library</h2>
-                <p className='ml-4'>I have enjoyed being able to contribute many new features and bugfixes on the CVS
-                    component library. I have learned a lot about libraries, versioning, and more from this experience!</p>
-                <h2 className='font-semibold ml-4 mt-4'>Diagnose and remove memory leak slowing test execution
-                    times</h2>
-                <p className='ml-4'>On a project with over 1500 front-end tests, I was able to find and successfully fix a
-                    memory leak improving test run time from 22 minutes to 3 - an <b>86% total reduction in test run
-                        time.</b></p>
-                <h2 className='font-semibold ml-4 mt-4'>Accessibility Champion</h2>
-                <p className='ml-4'>I have championed the effort to bring greater accessibility standards to all
-                    products in our lab. This has included sharing knowledge how to make apps more accessible,
-                    integrating better accessibility standards into story creation / general workflow, showcasing
-                    empathy building exercises, following
-                    <a className='underline hover:text-green-600 ml-1 mr-1'
-                       href='https://www.w3.org/TR/WCAG21/'
-                       rel='noreferrer'
-                       target='_blank'>W3 WCAG</a>
-                    standards and more.</p>
-                <h2 className='font-semibold ml-4 mt-4'>Upgrade security to Auth Code Flow</h2>
-                <p className='ml-4'>I took the initiative to upgrade the security for our application from Implicit to
-                    <a className='underline hover:text-green-600 ml-1'
-                       href='https://auth0.com/docs/flows/authorization-code-flow'
-                       rel='noreferrer'
-                       target='_blank'>Auth Code Flow</a>.
-                    Implementing this change involved work spanning from configuration on the front end
-                    to the back end as well as SSO setup and more. This was another great learning experience!</p>
+                <h1 className='font-semibold mt-10'>
+                    Technical Responsibilities and Information
+                </h1>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Technologies and Tools Used
+                </h2>
+                <p className='ml-4'>
+                    Typescript, Oracle SQL databases, GitLab, Flyway, Lombok,
+                    Hibernate, Spring Cloud Data Flow, Pivotal Tracker, Pivotal
+                    Cloud Foundry, Jenkins, Spring JPA
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Notable Libraries/Frameworks Used
+                </h2>
+                <p className='ml-4'>
+                    Spectator, NgRx, RxJS, AgGrid, JUnit, Mockito
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>Agile</h2>
+                <p className='ml-4'>
+                    I have been a part of an agile team since working at CVS.
+                    Some of the core concepts that I have been exposed to
+                    include TDD (test driven development), Continuous
+                    Integration/Continuous Deployment, paired programming and
+                    much more.
+                </p>
+
+                <h1 className='font-semibold mt-10'>
+                    Accomplishments and Other Roles
+                </h1>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Contribute to Proprietary Component Library
+                </h2>
+                <p className='ml-4'>
+                    I have enjoyed being able to contribute many new features
+                    and bugfixes on the CVS component library. I have learned a
+                    lot about libraries, versioning, and more from this
+                    experience!
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Diagnose and remove memory leak slowing test execution times
+                </h2>
+                <p className='ml-4'>
+                    On a project with over 1500 front-end tests, I was able to
+                    find and successfully fix a memory leak improving test run
+                    time from 22 minutes to 3 - an{' '}
+                    <b>86% total reduction in test run time.</b>
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Accessibility Champion
+                </h2>
+                <p className='ml-4'>
+                    I have championed the effort to bring greater accessibility
+                    standards to all products in our lab. This has included
+                    sharing knowledge how to make apps more accessible,
+                    integrating better accessibility standards into story
+                    creation / general workflow, showcasing empathy building
+                    exercises, following
+                    <a
+                        className='underline hover:text-green-600 ml-1 mr-1'
+                        href='https://www.w3.org/TR/WCAG21/'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        W3 WCAG
+                    </a>
+                    standards and more.
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Upgrade security to Auth Code Flow
+                </h2>
+                <p className='ml-4'>
+                    I took the initiative to upgrade the security for our
+                    application from Implicit to
+                    <a
+                        className='underline hover:text-green-600 ml-1'
+                        href='https://auth0.com/docs/flows/authorization-code-flow'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        Auth Code Flow
+                    </a>
+                    . Implementing this change involved work spanning from
+                    configuration on the front end to the back end as well as
+                    SSO setup and more. This was another great learning
+                    experience!
+                </p>
                 <h2 className='font-semibold ml-4 mt-4'>Give Presentations</h2>
-                <p className='ml-4'>I have given presentations in order to share knowledge with other engineers on
-                    various topics including
-                    <a className='underline hover:text-green-600 ml-1 mr-1'
-                       href='https://github.com/ngneat/spectator'
-                       rel='noreferrer'
-                       target='_blank'>Spectator</a>
+                <p className='ml-4'>
+                    I have given presentations in order to share knowledge with
+                    other engineers on various topics including
+                    <a
+                        className='underline hover:text-green-600 ml-1 mr-1'
+                        href='https://github.com/ngneat/spectator'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        Spectator
+                    </a>
                     and
-                    <a className='underline hover:text-green-600 ml-1'
-                       href='https://www.ag-grid.com/'
-                       rel='noreferrer'
-                       target='_blank'>AgGrid</a>
+                    <a
+                        className='underline hover:text-green-600 ml-1'
+                        href='https://www.ag-grid.com/'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
+                        AgGrid
+                    </a>
                     !
                 </p>
 
-                <h1 className='font-semibold mt-10'>Non-technical Responsibilities and Scope</h1>
+                <h1 className='font-semibold mt-10'>
+                    Non-technical Responsibilities and Scope
+                </h1>
                 <h2 className='font-semibold ml-4 mt-4'>Product Lifecycle</h2>
-                <p className='ml-4'>I have enjoyed a large degree of inclusion to every stage of the product lifecycle.
-                    I have been able
-                    to participate in the initial stages of new products and have been able to provide engineering
-                    insight and suggestions during Discovery and Framing sessions, Inceptions, and Design Studios. I
-                    have been able to directly interact with and to form professional relationships with our business
-                    partners. </p>
-                <h2 className='font-semibold ml-4 mt-4'>Conduct Interviews and Provide Mentorship</h2>
-                <p className='ml-4'>I have welcomed many new engineers and teammates and have taken on the role of a
-                    mentor in order to bring them up to speed in respect to code bases, practices, and our Agile rituals.
-                    Additionally, I have taken the responsibility of an interviewer and have evaluated prospective
-                    engineers and designers.</p>
+                <p className='ml-4'>
+                    I have enjoyed a large degree of inclusion to every stage of
+                    the product lifecycle. I have been able to participate in
+                    the initial stages of new products and have been able to
+                    provide engineering insight and suggestions during Discovery
+                    and Framing sessions, Inceptions, and Design Studios. I have
+                    been able to directly interact with and to form professional
+                    relationships with our business partners.{' '}
+                </p>
+                <h2 className='font-semibold ml-4 mt-4'>
+                    Conduct Interviews and Provide Mentorship
+                </h2>
+                <p className='ml-4'>
+                    I have welcomed many new engineers and teammates and have
+                    taken on the role of a mentor in order to bring them up to
+                    speed in respect to code bases, practices, and our Agile
+                    rituals. Additionally, I have taken the responsibility of an
+                    interviewer and have evaluated prospective engineers and
+                    designers.
+                </p>
             </div>
         </div>
-    );
+    )
 }
 
-export default Cvs;
+export default Cvs
--- src/routes/aboutMe/aboutMe.tsx
+++ src/routes/aboutMe/aboutMe.tsx
@@ -1,42 +1,57 @@
-import React, {useContext} from 'react';
-import './about-me-styles.scss';
-import ThemeContext from '../../theme-context';
-import {RESUME_LINK} from '../../App';
+import React, { useContext } from 'react'
+import './about-me-styles.scss'
+import ThemeContext from '../../theme-context'
+import { RESUME_LINK } from '../../App'
 
 const AboutMe = () => {
+    const theme = useContext(ThemeContext).theme
 
-    const theme = useContext(ThemeContext).theme;
-
-    const links: { label: string, link: string }[] = [
-        {label: 'Tools I use 🛠', link: 'https://github.com/pbarrett21/tools-and-gadgets/blob/main/README.md'},
-        {label: 'Github', link: 'https://github.com/pbarrett21'},
-        {label: 'LinkedIn', link: 'https://www.linkedin.com/in/pbarrett21/'},
-        {label: 'CodePen', link: 'https://codepen.io/paulbarrett'},
-        {label: 'Instagram', link: 'https://www.instagram.com/paulbarr21/'}
-    ];
+    const links: { label: string; link: string }[] = [
+        {
+            label: 'Tools I use 🛠',
+            link: 'https://github.com/pbarrett21/tools-and-gadgets/blob/main/README.md',
+        },
+        { label: 'Github', link: 'https://github.com/pbarrett21' },
+        { label: 'LinkedIn', link: 'https://www.linkedin.com/in/pbarrett21/' },
+        { label: 'CodePen', link: 'https://codepen.io/paulbarrett' },
+        { label: 'Instagram', link: 'https://www.instagram.com/paulbarr21/' },
+    ]
 
     return (
-        <div className={theme === 'dark' ?
-            'text-white max-w-full md:max-w-2xl lg:max-w-2xl m-4'
-            : 'max-w-full md:max-w-2xl lg:max-w-2xl m-4'}>
+        <div
+            className={
+                theme === 'dark'
+                    ? 'text-white max-w-full md:max-w-2xl lg:max-w-2xl m-4'
+                    : 'max-w-full md:max-w-2xl lg:max-w-2xl m-4'
+            }
+        >
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Career</h1>
             <p className='text-lg font-normal ml-8'>
-                I am a software developer with 3 years of professional experience. I
-                have been immersed in an agile development environment and have been fortunate to work with
-                fantastic teammates on many interesting projects while using modern technologies. I have
-                enjoyed being able to participate and contribute in the planning of new products and then to create and
-                bring that vision to life.
+                I am a software developer with 5 years of professional
+                experience. I have been immersed in an agile development
+                environment and have been fortunate to work with fantastic
+                teammates on many interesting projects while using modern
+                technologies. I have enjoyed being able to participate and
+                contribute in the planning of new products and then to create
+                and bring that vision to life.
             </p>
 
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Education</h1>
             <div>
-                <p className='text-lg font-medium ml-8'>B.S. in Computer Science with Minor in Mathematics</p>
-                <p className='text-lg font-normal ml-8'>University of Kentucky, 2015 - 2019</p>
+                <p className='text-lg font-medium ml-8'>
+                    B.S. in Computer Science with Minor in Mathematics
+                </p>
+                <p className='text-lg font-normal ml-8'>
+                    University of Kentucky, 2015 - 2019
+                </p>
             </div>
 
-            <h1 className='font-semibold text-2xl mt-10 mb-6'>Hobbies and Interests</h1>
+            <h1 className='font-semibold text-2xl mt-10 mb-6'>
+                Hobbies and Interests
+            </h1>
             <p className='text-lg font-normal ml-8'>
-                I have many hobbies and interests outside of my professional life!
+                I have many hobbies and interests outside of my professional
+                life!
             </p>
             <ul className='pt-4 pl-16'>
                 <li>Mountain Biking 🚴</li>
@@ -51,24 +66,34 @@ const AboutMe = () => {
                 Here are some useful or relevant links:
             </p>
             <ul className='pt-4 pl-16'>
-                {links.map(link => (
-                    <li key={link.label} className='mb-1'><a className='underline hover:text-green-600'
-                                                             href={link.link}
-                                                             rel='noreferrer'
-                                                             target='_blank'>{link.label}</a></li>
+                {links.map((link) => (
+                    <li key={link.label} className='mb-1'>
+                        <a
+                            className='underline hover:text-green-600'
+                            href={link.link}
+                            rel='noreferrer'
+                            target='_blank'
+                        >
+                            {link.label}
+                        </a>
+                    </li>
                 ))}
             </ul>
 
             <h1 className='font-semibold text-2xl mt-10 mb-6'>Resume</h1>
             <p className='text-lg font-normal ml-8'>
                 Feel free to take a look at my
-                <a className='font-medium underline ml-1 hover:text-green-600'
-                   href={RESUME_LINK}
-                   rel='noreferrer'
-                   target='_blank'>resume</a>
+                <a
+                    className='font-medium underline ml-1 hover:text-green-600'
+                    href={RESUME_LINK}
+                    rel='noreferrer'
+                    target='_blank'
+                >
+                    resume
+                </a>
             </p>
         </div>
-    );
+    )
 }
 
-export default AboutMe;
+export default AboutMe
